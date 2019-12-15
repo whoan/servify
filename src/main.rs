ed0abb890414e8e7a4dce5f867653e10a661beaa
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-12-15 09:59:38 -0300

    Parse arguments as options

--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,48 @@ extern crate iron;
 extern crate rustc_serialize as serialize;
 extern crate valico;
 
+#[macro_use]
+extern crate clap;
+
 use rustless::{
     Application, Api, Nesting
 };
 use rustless::json::ToJson;
 
+/// Call any command as a service
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "Juan Abadie")]
+struct Opts {
+    /// Command to be called as a service
+    command: String,
+
+    /// [Optional] URI for the service
+    #[clap(short = "u", long = "uri", default_value = "/")]
+    uri: String,
+
+    /// [Optional] HTTP method for the service
+    #[clap(short = "m", long = "method", default_value = "GET")]
+    method: String
+}
+
 fn main() {
 
-    let arguments: Vec<String> = std::env::args().collect();
-    let url = if arguments.len() > 1 {
-        &arguments[1]
-    } else {
-        "/"
-    };
+    let opts: Opts = Opts::parse();
+    println!("Command: {}", opts.command);
+    println!("Uri: {}", opts.uri);
+    println!("Method: {}", opts.method);
+
+    let uri = opts.uri;
 
     let api = Api::build(|api| {
 
         // Create API according to arument
         api.mount(Api::build(|chats_api| {
 
-            chats_api.get(url, |endpoint| {
+            chats_api.get(&uri, |endpoint| {
 
                 // Add description
-                endpoint.desc(&( "Get ".to_owned() + &url));
+                endpoint.desc(&( "Get ".to_owned() + &uri));
 
                 endpoint.handle(|client, params| {
                     client.json(&params.to_json())
@@ -38,7 +57,6 @@ fn main() {
 
     let app = Application::new(api);
 
-    println!("Rustless server started! on 4000");
+    println!("Rustless server started! on http://0.0.0.0:4000/{}", &uri);
     iron::Iron::new(app).http("0.0.0.0:4000").unwrap();
-
 }
